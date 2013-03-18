ba7fbad88d07a88de58962028c40efed15de41b6
Wuilder Alvarado <walvarado@researchmobile.co>
2013-03-18 16:24:23 -0600

    Recibelo. Colores de Estados
    Se implemento el proceso de mostrar el color correspondiente al estado
    de la asignación en Recibelo

--- Coretel/src/com/researchmobile/supervisionpasalo/view/Asignaciones.java
+++ Coretel/src/com/researchmobile/supervisionpasalo/view/Asignaciones.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -26,10 +26,11 @@ public class Asignaciones extends Activity {
 	private CatalogoAsignacion catalogoAsignacion;
 	private AnotacionAsignacion anotacionAsignacion;
 	
-	private ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+	private ArrayList<HashMap<String, Object>> mylist = new ArrayList<HashMap<String, Object>>();
 	
 		protected void onCreate(Bundle savedInstanceState)
 		{
+			
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.listas_supervision);
 			Bundle bundle = getIntent().getExtras();
@@ -47,49 +48,35 @@ public class Asignaciones extends Activity {
 			int tamano = getCatalogoAsignacion().getAnotacionasignacion().length;
 			for(i=0; i<tamano; i++)
 				{
-					HashMap<String, String>map = new HashMap<String,String>();
+				
+					HashMap<String, Object>map = new HashMap<String,Object>();
 	                map.put("creacion",getCatalogoAsignacion().getAnotacionasignacion()[i].getFormat_fecha_asignado());		
 	                map.put("asignado",getCatalogoAsignacion().getAnotacionasignacion()[i].getFormat_fecha_asignado());
 	                map.put("resultado",getCatalogoAsignacion().getAnotacionasignacion()[i].getFecha_registro());
 	                map.put("comunidad", getCatalogoAsignacion().getAnotacionasignacion()[i].getNombreComunidad());
 	                map.put("tipo", getCatalogoAsignacion().getAnotacionasignacion()[i].getNombreTipoAnotacion());
+	                int estado = getCatalogoAsignacion().getAnotacionasignacion()[i].getId_estado();
+	                if (estado == 1){
+	                	estado = this.getResources().getIdentifier("estado_1", "drawable", this.getPackageName());
+	                }else if (estado == 2){
+	                	estado = this.getResources().getIdentifier("estado_2", "drawable", this.getPackageName());
+	                }else if (estado == 3){
+	                	estado = this.getResources().getIdentifier("estado_3_4", "drawable", this.getPackageName());
+	                }else if (estado == 4){
+	                	estado = this.getResources().getIdentifier("estado_3_4", "drawable", this.getPackageName());
+	                }
+	                map.put("estado", estado);
 	                mylist.add(map);
 				}
 			setSimpleAdapter(new SimpleAdapter(this,
-//					datos.Data(),//Data es el nombre que la clase Datos al final del public static ArrayList
 					mylist,
 					R.layout.asignaciones_supervision,
-					new String[]{"creacion","asignado","resultado","comunidad","tipo"},
-					new int[]{R.id.asignaciones_creacion_textview,R.id.asignaciones_asignacion_textview,R.id.asignaciones_resuelto_textview,R.id.asignaciones_comunidad_textview,R.id.asignaciones_tipo_textview}));
+					new String[]{"creacion","asignado","resultado","comunidad","tipo", "estado"},
+					new int[]{R.id.asignaciones_creacion_textview,R.id.asignaciones_asignacion_textview,R.id.asignaciones_resuelto_textview,R.id.asignaciones_comunidad_textview,R.id.asignaciones_tipo_textview, R.id.asignaciones_estado_imageview}));
 							
 					getListadoListView().setAdapter(getSimpleAdapter());
 					
-	         /*
-			map.put("Create", "02/02/2013 10:19 a.m");
-			map.put("Asignate", "05/02/2013 8:00 a.m");
-			map.put("Result", "06/02/2013 8:00 a.m");
-			map.put("Comunity", "RSOLARES");
-			map.put("Tipe", "Tipo1");
-			mylist.add(map);
-			map = new HashMap<String, String>();
-			map.put("Create", "02/03/2013 10:19 a.m");
-			map.put("Asignate", "05/03/2013 8:00 a.m");
-			map.put("Result", "06/03/2013 8:00 a.m");
-			map.put("Comunity", "RSOLARES");
-			map.put("Tipe", "Tipo2");
-			mylist.add(map);
-			map = new HashMap<String, String>();
-			map.put("Create", "02/04/2013 10:19 a.m");
-			map.put("Asignate", "05/04/2013 8:00 a.m");
-			map.put("Result", "06/04/2013 8:00 a.m");
-			map.put("Comunity", "RSOLARES");
-			map.put("Tipe", "Tipo3");
-			mylist.add(map);
-			*/
-			
-			//LISTADO
-					
-	}
+		}
 
 
 		
@@ -136,13 +123,13 @@ public class Asignaciones extends Activity {
 
 
 
-		public ArrayList<HashMap<String, String>> getMylist() {
+		public ArrayList<HashMap<String, Object>> getMylist() {
 			return mylist;
 		}
 
 
 
-		public void setMylist(ArrayList<HashMap<String, String>> mylist) {
+		public void setMylist(ArrayList<HashMap<String, Object>> mylist) {
 			this.mylist = mylist;
 		}
 
