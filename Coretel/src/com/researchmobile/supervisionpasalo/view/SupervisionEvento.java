b11c1c4731f2266e7aa2372b22efbd46bd782afd
Randy Solares <rsolares@researchmobile.co>
2013-03-19 17:19:46 -0600

    Carga Imagen en SupervisionEvento

--- Coretel/src/com/researchmobile/supervisionpasalo/view/SupervisionEvento.java
+++ Coretel/src/com/researchmobile/supervisionpasalo/view/SupervisionEvento.java
@@ -1,14 +1,26 @@
 package com.researchmobile.supervisionpasalo.view;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
 import com.researchmobile.coretel.utility.TokenizerUtility;
 import com.researchmobile.coretel.view.R;
@@ -20,10 +32,10 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 	private TextView tipoTextView;
 	private TextView descripcionTextView;
 	private TextView comunidadTextView;
-	private ImageView eventofotoImageView;
+	private ImageView asignacionfotoImageView;
 	private Button responderButton;
 	private Button regresarButton;
-	
+	private String descripcion = "";
 	private TokenizerUtility tokenizer = new TokenizerUtility();
 	
 		protected void onCreate(Bundle savedInstanceState)
@@ -32,7 +44,7 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 			setContentView(R.layout.supervision_evento);
 			Bundle bundle = getIntent().getExtras();
 			String fecha = bundle.getString("titulo");
-			String descripcion = bundle.getString("descripcion");
+			descripcion = bundle.getString("descripcion");
 			String latitud = bundle.getString("latitud");
 			String longitud = bundle.getString("longitud");
 			Log.v("coretel", "titulo = " + fecha);
@@ -48,7 +60,7 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 			setComunidadTextView((TextView)findViewById(R.id.evento_comunidadTextView));
 			setResponderButton((Button)findViewById(R.id.evento_responderbutton));
 			setRegresarButton((Button)findViewById(R.id.evento_regresarButton));
-			setEventoFotoImageView((ImageView)findViewById(R.id.evento_fotoImageView));
+			setAsignacionfotoImageView((ImageView)findViewById(R.id.supervisionevento_fotoImageView));
 			
 			getResponderButton().setOnClickListener(this);
 			getRegresarButton().setOnClickListener(this);
@@ -63,6 +75,7 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 			//utilizando tokenizer
 			getFechaTextView().setText(tokenizer.fechaRegistro(descripcion));
 			getDescripcionTextView().setText(tokenizer.descripcion(descripcion));
+			MostrarImagen();
 		}
 		
 			public void onClick(View view){
@@ -74,6 +87,70 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 				}
 			}
 		
+	//metodo para cargar imagenes
+			private void MostrarImagen() {
+				System.out.println("Variable descripcion" + descripcion);
+				String url = "http://23.23.1.2/WS/" + tokenizer.imagen(descripcion);
+				System.out.println("Recibe imagen");
+				Log.e("TT", "url imagen = " + url);
+				new descargaImagenes().execute(url);
+			getAsignacionfotoImageView().setVisibility(View.VISIBLE);
+		}
+
+	//descargaImagenes
+			class descargaImagenes extends AsyncTask<String, Integer, Integer>{
+
+				ProgressBar miBarra;
+				
+				@Override
+				protected void onPreExecute(){
+					miBarra = (ProgressBar)findViewById(R.id.barraProgreso);
+					
+				}
+				@Override
+				protected Integer doInBackground(String... urlString) {
+					try {
+						URL url = new URL(urlString[0]);
+						URLConnection conexion = url.openConnection();
+						conexion.connect();
+						
+						int tamano = conexion.getContentLength();
+						Log.e("TT", "resultado busqueda imagen = " + tamano);
+						InputStream input = new BufferedInputStream(url.openStream());
+						OutputStream output = new FileOutputStream("/sdcard/temp.jpg");
+						
+						byte datos[] = new byte[1024];
+						int cuenta;
+						long total = 0;
+						
+						while ((cuenta = input.read(datos)) != -1){
+							total += cuenta;
+							output.write(datos, 0, cuenta);
+							
+							this.publishProgress((int)total*100/tamano);
+						}
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return null;
+				}
+				
+				@Override
+				protected void onProgressUpdate(Integer...progreso){
+					int porcentaje = progreso[0];
+					miBarra.setProgress(porcentaje);
+				}
+				
+				protected void onPostExecute(Integer resultado){
+					getAsignacionfotoImageView().setImageBitmap(BitmapFactory.decodeFile("/sdcard/temp.jpg"));
+				}
+			}
+			
+			
 		
 		private void Responder() {
 			Intent intent = new Intent(SupervisionEvento.this, SupervisionRespuesta.class);
@@ -81,16 +158,11 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 			}
 		
 		private void Retorno(){
-			Intent intent = new Intent(SupervisionEvento.this, MapaSupervision.class );
-			startActivity(intent);
-		}
-
-		public ImageView getEventoFotoImageView(){
-			return eventofotoImageView;
-		}
-		
-		public void setEventoFotoImageView(ImageView EventoFotoImageView){
-			this.eventofotoImageView = eventofotoImageView;
+			float latitud = (float) 14.627853;
+			float longitud = (float) -90.517584;
+			Intent intent = new Intent(SupervisionEvento.this, MapaSupervision.class);
+			intent.putExtra("latitud", latitud);
+			intent.putExtra("longitud", longitud);
 		}
 
 		public TextView getFechaTextView() {
@@ -133,16 +205,18 @@ public class SupervisionEvento extends Activity implements OnClickListener {
 			this.comunidadTextView = comunidadTextView;
 		}
 
-		public ImageView getEventofotoImageView() {
-			return eventofotoImageView;
-		}
-
-		public void setEventofotoImageView(ImageView eventofotoImageView) {
-			this.eventofotoImageView = eventofotoImageView;
-		}
-
 		public Button getResponderButton() {
 			return responderButton;
+		}
+		
+		
+
+		public ImageView getAsignacionfotoImageView() {
+			return asignacionfotoImageView;
+		}
+
+		public void setAsignacionfotoImageView(ImageView asignacionfotoImageView) {
+			this.asignacionfotoImageView = asignacionfotoImageView;
 		}
 
 		public void setResponderButton(Button responderButton) {
