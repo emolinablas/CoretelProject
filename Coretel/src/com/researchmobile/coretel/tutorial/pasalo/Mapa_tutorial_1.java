42de871477751d3965aec4df921f1fb4692f7c09
Ever Molina <moulinaem@gmail.com>
2013-04-03 09:11:32 -0600

    Creacion de layout tutorial mapa

--- Coretel/src/com/researchmobile/coretel/tutorial/pasalo/Mapa_tutorial_1.java
+++ Coretel/src/com/researchmobile/coretel/tutorial/pasalo/Mapa_tutorial_1.java
@@ -1,5 +1,15 @@
 package com.researchmobile.coretel.tutorial.pasalo;
 
-public class Mapa_tutorial_1 {
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Mapa_tutorial_1 extends Activity implements OnClickListener {
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
